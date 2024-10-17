1e56cab8ac4b58c1c5cafd554399ed1effe5e68b
baish <baish@BAISHNABI>
2024-10-17 13:27:50 +0530

    Add project files.

---
+++ MergeTwoSortedLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+public class ll {
+    static void Main(string[] args)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.AddLast(10);
+        list.AddLast(20);
+        list.AddLast(30);
+        LinkedList<int> list2 = new LinkedList<int>();
+        list2.AddLast(50);
+        list2.AddLast(60);
+        LinkedList<int> list3 = new LinkedList<int>();
+        foreach (int i in list)
+        {
+            list3.AddLast(i);
+            Console.WriteLine(i);
+        }
+        foreach (int i in list2)
+        {
+            list.AddLast(i);
+            Console.WriteLine(i);
+        }
+    }
+}
